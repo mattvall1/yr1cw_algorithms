e6a97ca9945fc7e723ffbe36bf42c74a236b8278
kharmsworth <123576152+kharmsworth@users.noreply.github.com>
2023-02-27 17:59:06 +0000

    Create establishingQuadrants.py

---
+++ establishingQuadrants.py
@@ -0,0 +1,40 @@
+"""
+Author: Kayleigh Harmsworth
+Description: Script to establish the quadrants needed for the coursework
+Date: 27/02/23
+"""
+#upper left quadrant
+upper_left = []
+if x < 0 and y > 0:
+    upper_left.append()
+    upper_left.append()
+
+#upper right quadrant
+upper_right = []
+if x > 0 and y > 0:
+    upper_right.append()
+
+#lower left quadrant
+lower_left = []
+if x < 0 and y < 0:
+    lower_left.append()
+
+#lower right quadrant
+lower_right = []
+if x > 0 and y < 0:
+    lower_right.append()
+
+#x-axis
+x_axis = []
+if y = 0:
+    x_axis.append()
+
+#y-axis
+y_axis = []
+if x = 0:
+    y_axis.append()
+
+#origin
+origin = []
+if x = 0 and y = 0:
+    origin.append()
