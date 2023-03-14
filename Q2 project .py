679d614f49e558a5450212397fbf0f645ce8ec3c
Matthew Vallance <matthew.vallance@outlook.com>
2023-03-14 10:25:59 +0000

    Basil solution - 1st draft

---
+++ Q2 project .py
@@ -0,0 +1,44 @@
+
+def get_quadrant(coords):
+    x1, y1 = coords[0]
+    x2, y2 = coords[1]
+
+    if x1 == 0 and y1 == 0:
+        quad1 = "Point 1 is at the origin"
+    elif x1 == 0:
+        quad1 = "Point 1 is on the y-axis"
+    elif y1 == 0:
+        quad1 = "Point 1 is on the x-axis"
+    elif x1 > 0 and y1 > 0:
+        quad1 = "Point 1 is in quadrant 1"
+    elif x1 < 0 and y1 > 0:
+        quad1 = "Point 1 is in quadrant 2"
+    elif x1 < 0 and y1 < 0:
+        quad1 = "Point 1 is in quadrant 3"
+    elif x1 > 0 and y1 < 0:
+        quad1 = "Point 1 is in quadrant 4"
+    else:
+     quad1 = "Invalid coordinates"
+ 
+    if x2 == 0 and y2 == 0:
+        quad2 = "Point 2 is at the origin"
+    elif x2 == 0:
+        quad2 = "Point 2 is on the y-axis"
+    elif y2 == 0:
+        quad2 = "Point 2 is on the x-axis"
+    elif x2 > 0 and y2 > 0:
+        quad2 = "Point 2 is in quadrant 1"
+    elif x2 < 0 and y2 > 0:
+        quad2 = "Point 2 is in quadrant 2"
+    elif x2 < 0 and y2 < 0:
+        quad2 = "Point 2 is in quadrant 3"
+    elif x2 > 0 and y2 < 0:
+        quad2 = "Point 2 is in quadrant 4"
+    else:
+        quad2 = "Invalid coordinates"
+ 
+    return f"{quad1}, {quad2}"
+
+
+coords = ((-2, 3), (4, -5))
+print(get_quadrant(coords))
